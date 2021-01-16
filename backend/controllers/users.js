56fca63cea76efe4889d77b99d88ac2296e9fd29
Natalia Popova <cindy.n-popova@yandex.ru>
2021-01-16 22:06:29 +0300

    user login in controllers created

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,7 @@
 const user = require('../models/user');
 const { showError, OK_CODE } = require('../helper/helper');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // показать всех пользователей
 const getUsers = (req, res) => user.find({})
@@ -15,6 +16,27 @@ const getUser = (req, res) => {
     .then((userData) => res.status(OK_CODE).send(userData))
     .catch((err) => showError(res, err));
 };
+
+const login = (req, res) => {
+  const {email, password} = req.body;
+  user.findOne({ email })
+    .then(user => {
+      if(!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then(matched => {
+          if(!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          const token = jwt.sign({ _id: user._id }, 'some-secret-key',  { expiresIn: '7d' });
+          res.cookie('jwt',token, { httpOnly: true, maxAge: 3600000 });
+        })
+    })
+    .catch(err => {
+      res.status(401).send({message: err.message});
+    });
+}
 
 // создать нового пользователя
 const createUser = (req, res) =>
